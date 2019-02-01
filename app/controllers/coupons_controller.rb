d20448c4ee85954a6281fe5e0b041aac28fcca59
Ben Yellin <byellindc@gmail.com>
2019-02-01 11:57:48 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,28 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    get_coupon
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    coupon = Coupon.create(coupon_params)
+    redirect_to coupon
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:coupon_code, :store)
+  end
+
+  def get_coupon
+    @coupon = Coupon.find(params[:id])
+  end
+end
